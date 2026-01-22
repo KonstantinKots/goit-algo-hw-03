ab3cd92633d78798bb4ade2b4ef293aa8f6df41c
Konstantin K <kim9@i.ua>
2026-01-22 19:49:04 +0200

    ++new comment

--- first_task.py
+++ first_task.py
@@ -8,7 +8,7 @@ def get_days_from_today(date: str) -> int:
     '''
     Функція розраховує цілу кількість днів між сьогодняшньою та заданою датами
     
-    :param date: рядок з датою в форматі YYYY.MM.DD
+    :param date: рядок з датою в форматі YYYY-MM-DD
     :type date: str
     :return: різницю в днях або None якщо невірний формат дати
     :rtype: int
@@ -16,12 +16,12 @@ def get_days_from_today(date: str) -> int:
     
     try:
         #перетворюємо рядок у об'єкт date
-        convert_date = datetime.strptime(date, '%Y.%m.%d').date()
+        convert_date = datetime.strptime(date, '%Y-%m-%d').date()
         delta_days = today - convert_date
         return delta_days.days                                  
     except ValueError:
         #обробка невірного формату
-        print("Enter, please, the date in the format: YYYY.MM.DD")
+        print("Enter, please, the date in the format: YYYY-MM-DD")
         return None
 #викликаємо функцію    
 result = get_days_from_today(date_str)

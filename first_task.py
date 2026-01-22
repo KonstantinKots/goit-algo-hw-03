b05c2e32fe6ff25e5cfa51321f50b20726206638
Konstantin K <kim9@i.ua>
2026-01-22 19:54:17 +0200

    +++

--- first_task.py
+++ first_task.py
@@ -1,8 +1,8 @@
 
 from datetime import datetime
 
-date_str = input("Enter the date (year.month.day): ")     
-today = datetime.now().date()                               
+date_str = input("Enter the date (year.month.day): ")
+today = datetime.now().date()
 
 def get_days_from_today(date: str) -> int:
     '''
@@ -13,16 +13,15 @@ def get_days_from_today(date: str) -> int:
     :return: різницю в днях або None якщо невірний формат дати
     :rtype: int
     '''
-    
     try:
         #перетворюємо рядок у об'єкт date
         convert_date = datetime.strptime(date, '%Y-%m-%d').date()
         delta_days = today - convert_date
-        return delta_days.days                                  
+        return delta_days.days
     except ValueError:
         #обробка невірного формату
         print("Enter, please, the date in the format: YYYY-MM-DD")
         return None
-#викликаємо функцію    
+#викликаємо функцію
 result = get_days_from_today(date_str)
-print(result)
+print(f"Різниця між датами складає: {result}")

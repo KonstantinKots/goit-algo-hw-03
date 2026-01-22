29098ae45a2b1c44a0037b9ec870aa775e4f4370
Konstantin K <kim9@i.ua>
2026-01-22 19:03:16 +0200

    documented code

--- first_task.py
+++ first_task.py
@@ -1,19 +1,27 @@
 
-from datetime import datetime                 
+from datetime import datetime
 
-date_str = input("Enter the date (year.month.day): ")     #введення данних користувачем
-today = datetime.now().date                               #отримуємо сьогодняшню дату без часу
+date_str = input("Enter the date (year.month.day): ")     
+today = datetime.now().date()                               
 
-def get_days_from_today(date):                            #створюємо функцію для розрахунку різниці між датами
-    try:
-        convert_date = datetime.strptime(date, '%Y.%m.%d').date() #перетворюємо сторку у обєкт datetime
-        delta_days = today() - convert_date                       #розраховуємо різницю між датами
-        return delta_days.days                                    #повертаємо результат різниці між датами
-    except:
-        print("Enter, please, the date in the format: YYYY.MM.DD")  #обробка невірного введення формата дати
-        
-
-result = get_days_from_today(date_str)   #викликаємо функцію
-print(result)                            
+def get_days_from_today(date: str) -> int:
+    '''
+    Функція розраховує цілу кількість днів між сьогодняшньою та заданою датами
     
-
+    :param date: рядок з датою в форматі YYYY.MM.DD
+    :type date: str
+    :return: різницю в днях або None якщо невірний формат дати
+    :rtype: int
+    '''
+    
+    try:
+        #перетворюємо рядок у об'єкт date
+        convert_date = datetime.strptime(date, '%Y.%m.%d').date()
+        delta_days = today - convert_date
+        return delta_days.days                                  
+    except ValueError:
+        print("Enter, please, the date in the format: YYYY.MM.DD")
+        return None
+#викликаємо функцію    
+result = get_days_from_today(date_str)
+print(result)
